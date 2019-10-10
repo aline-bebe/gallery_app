baa06351df292570597d11fc79ab772ac8c914b1
aline-bebe <ingabirealine40@gmail.com>
2019-10-10 13:07:28 +0200

    difine a special roote that will help to upload our images

--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,24 @@
+
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import HttpResponse
+from .models import Image,Category,Location
 
 # Create your views here.
+
 def welcome(request):
-    return HttpResponse('Welcome to my Gallery')
+    images = Image.objects.all()
+    return render(request, 'all-images/index.html',{"images":images,})
+
+
+def search_results(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get("category")
+        searched_categories = Image.search_by_category(search_term)
+        
+        message = f"{search_term}"
+        
+        return render(request, 'all-images/search.html',{"message":message,"categories":searched_categories})
+    
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html',{"message":message,"categories":searched_categories})
